e6c6939522c748b063b041f31cedd7e65c04f89f
S4NDM4NN <travissandmann@gmail.com>
2020-07-17 15:40:48 -0500

    make batch files run dos2unix first

--- docker-scripts/WIN/WIN-COMPILE-CGAME.bat
+++ docker-scripts/WIN/WIN-COMPILE-CGAME.bat
@@ -1,3 +1,4 @@
 cd ../..
-docker run -it --rm -v %cd%:/rtcwpro rtcwpro:build /rtcwpro/docker-scripts/build/build-cgame.sh && ./rtcwpro/docker-scripts/build/build-cgame.sh
+docker run -it --rm -v %cd%:/rtcwpro rtcwpro:build dos2unix/rtcwpro/docker-scripts/build/build-cgame.sh 
+docker run -it --rm -v %cd%:/rtcwpro rtcwpro:build ./rtcwpro/docker-scripts/build/build-cgame.sh
 pause
--- docker-scripts/WIN/WIN-COMPILE-PK3.bat
+++ docker-scripts/WIN/WIN-COMPILE-PK3.bat
@@ -1,3 +1,4 @@
 cd ../..
-docker run -it --rm -v %cd%:/rtcwpro rtcwpro:build /rtcwpro/docker-scripts/build/build-pk3.sh && ./rtcwpro/docker-scripts/build/build-pk3.sh
+docker run -it --rm -v %cd%:/rtcwpro rtcwpro:build dos2unix /rtcwpro/docker-scripts/build/build-pk3.sh 
+docker run -it --rm -v %cd%:/rtcwpro rtcwpro:build ./rtcwpro/docker-scripts/build/build-pk3.sh
 pause
