c8e143c41e013d6a24759a2439430b172ec8272e
Eamon Woortman <eamon.woortman@gmail.com>
2020-07-18 10:34:13 +0200

    Switched to absolute paths and made sure the output folder exists before building

--- docker-scripts/build/build-pk3.sh
+++ docker-scripts/build/build-pk3.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
+
 PK3_NAME=rtcwpro.pk3
+OUTPUT_FOLDER=/rtcwpro/output
+CONTENT_FOLDER=/rtcwpro/MAIN
+
+if [ ! -d "$OUTPUT_FOLDER" ]; then
+    echo "Output folder does not exist: $OUTPUT_FOLDER"
+    exit;
+fi
 
 echo '############################################################################################'
 echo '##################################    BUILD PK3     ########################################'
 echo '############################################################################################'
-cd /rtcwpro/MAIN
-zip -r ../output/$PK3_NAME ./*
-cd ../output
-zip -r $PK3_NAME ./ui*.dll ./cgame*.dll ./ui*.so ./cgame*.so
+rm -f $OUTPUT_FOLDER/$PK3_NAME
+cd $CONTENT_FOLDER && zip -r $OUTPUT_FOLDER/$PK3_NAME ./* && cd -
+zip -j -r $OUTPUT_FOLDER/$PK3_NAME $OUTPUT_FOLDER/ui*.dll $OUTPUT_FOLDER/cgame*.dll $OUTPUT_FOLDER/ui*.so $OUTPUT_FOLDER/cgame*.so
