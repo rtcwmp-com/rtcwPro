8a7b38a90d3122ffce8220451154c27c43b9a0d8
krazykaze81 <mike97gt@gmail.com>
2020-07-20 16:20:33 -0400

    moved copydll file to docker folder
    moved copydll file to docker folder

---
+++ docker-scripts/WIN/copyDlls.bat
@@ -0,0 +1,9 @@
+copy "J:\GitHub\ospx-elite\output\qagame_mp_x86.dll" "C:\Program Files (x86)\Return to Castle Wolfenstein\rtcwpro" /Y
+copy "J:\GitHub\ospx-elite\output\cgame_mp_x86.dll" "C:\Program Files (x86)\Return to Castle Wolfenstein\rtcwpro" /Y
+copy "J:\GitHub\ospx-elite\output\ui_mp_x86.dll" "C:\Program Files (x86)\Return to Castle Wolfenstein\rtcwpro" /Y
+copy "J:\GitHub\ospx-elite\output\rtcwpro.pk3" "C:\Program Files (x86)\Return to Castle Wolfenstein\rtcwpro" /Y
+
+copy "J:\GitHub\ospx-elite\output\qagame_mp_x86.dll" "C:\Program Files (x86)\Return to Castle Wolfenstein Copy\rtcwpro" /Y
+copy "J:\GitHub\ospx-elite\output\cgame_mp_x86.dll" "C:\Program Files (x86)\Return to Castle Wolfenstein Copy\rtcwpro" /Y
+copy "J:\GitHub\ospx-elite\output\ui_mp_x86.dll" "C:\Program Files (x86)\Return to Castle Wolfenstein Copy\rtcwpro" /Y
+copy "J:\GitHub\ospx-elite\output\rtcwpro.pk3" "C:\Program Files (x86)\Return to Castle Wolfenstein Copy\rtcwpro" /Y
