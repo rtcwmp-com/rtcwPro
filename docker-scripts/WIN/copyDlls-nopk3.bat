8cbcb255dd39652e808d292dc648d3988c7e9855
krazykaze81 <mike97gt@gmail.com>
2020-10-29 13:00:03 -0400

    menu fixes for map restart and reset match

---
+++ docker-scripts/WIN/copyDlls-nopk3.bat
@@ -0,0 +1,22 @@
+copy "J:\GitHub\RTCW-Pro\output\qagame_mp_x86.dll" "J:\GitHub\Rtcw-Pro\src\rtcwpro\" /Y
+copy "J:\GitHub\RTCW-Pro\output\cgame_mp_x86.dll" "J:\GitHub\RTCW-Pro\src\rtcwpro\" /Y
+copy "J:\GitHub\RTCW-Pro\output\ui_mp_x86.dll" "J:\GitHub\RTCW-Pro\src\rtcwpro\" /Y
+copy "J:\GitHub\RTCW-Pro\output\qagame_mp_x86.dll" "C:\Program Files (x86)\Return to Castle Wolfenstein\rtcwpro" /Y
+copy "J:\GitHub\RTCW-Pro\output\cgame_mp_x86.dll" "C:\Program Files (x86)\Return to Castle Wolfenstein\rtcwpro" /Y
+copy "J:\GitHub\RTCW-Pro\output\ui_mp_x86.dll" "C:\Program Files (x86)\Return to Castle Wolfenstein\rtcwpro" /Y
+
+copy "J:\GitHub\RTCW-Pro\output\qagame_mp_x86.dll" "C:\Program Files (x86)\Return to Castle Wolfenstein Copy\rtcwpro" /Y
+copy "J:\GitHub\RTCW-Pro\output\cgame_mp_x86.dll" "C:\Program Files (x86)\Return to Castle Wolfenstein Copy\rtcwpro" /Y
+copy "J:\GitHub\RTCW-Pro\output\ui_mp_x86.dll" "C:\Program Files (x86)\Return to Castle Wolfenstein Copy\rtcwpro" /Y
+
+rem cd ../..
+rem docker run -it --rm -v %cd%:/rtcwpro rtcwpro:build dos2unix /rtcwpro/docker-scripts/build/build-pk3.sh 
+rem docker run -it --rm -v %cd%:/rtcwpro rtcwpro:build ./rtcwpro/docker-scripts/build/build-pk3.sh
+
+copy "J:\GitHub\RTCW-Pro\output\qagame_mp_x86.dll" "J:\GitHub\RTCW-Pro\src\rtcwpro" /Y
+copy "J:\GitHub\RTCW-Pro\output\cgame_mp_x86.dll" "J:\GitHub\RTCW-Pro\src\rtcwpro" /Y
+copy "J:\GitHub\RTCW-Pro\output\ui_mp_x86.dll" "J:\GitHub\RTCW-Pro\src\rtcwpro" /Y
+copy "J:\GitHub\RTCW-Pro\output\rtcwpro.pk3" "J:\GitHub\RTCW-Pro\src\rtcwpro" /Y
+copy "J:\GitHub\RTCW-Pro\output\rtcwpro.pk3" "C:\Program Files (x86)\Return to Castle Wolfenstein\rtcwpro" /Y
+copy "J:\GitHub\RTCW-Pro\output\rtcwpro.pk3" "C:\Program Files (x86)\Return to Castle Wolfenstein Copy\rtcwpro" /Y
+pause
