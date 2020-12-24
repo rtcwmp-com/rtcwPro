ef00808e05ee44e5b4cee725e784f5faa2e7d9cc
Greg <nihilist314@outlook.com>
2020-12-24 15:47:16 -0800

    fix cvar restriction lag on map_restart/round start. will provide a more elegant solution soon

--- src/game/g_svcmds.c
+++ src/game/g_svcmds.c
@@ -750,7 +750,7 @@ void Svcmd_ResetMatch_f(qboolean fDoReset, qboolean fDoRestart) {
 	level.alliedSniper = level.axisSniper = 0;
 	level.alliedPF = level.axisPF = 0;
 	level.alliedVenom = level.axisVenom = 0;
-	
+
 
 	if (fDoReset) {
 		G_resetRoundState();
@@ -959,10 +959,10 @@ G_UpdateSvCvars
 void G_UpdateSvCvars(void)
 {
 	char cs[MAX_INFO_STRING];
+	//char cs[BIG_INFO_STRING];
 	int  i;
 
 	cs[0] = '\0';
-
 	for (i = 0; i < level.svCvarsCount; i++)
 	{
 		if (level.svCvars[i].Val2[0] == 0) // don't send a space char when not set
@@ -981,6 +981,7 @@ void G_UpdateSvCvars(void)
 
 	// FIXME: print a warning when this configstring has nearly reached MAX_INFO_STRING size and don't set it if greater
 	trap_SetConfigstring(CS_SVCVAR, cs);
+
 }
 
 /*
@@ -1017,6 +1018,7 @@ void CC_svcvar(void)
 		G_Printf("usage: sv_cvar <cvar name> <mode> <value1> <value2>\nexamples: sv_cvar r_rmse EQ 0\n          sv_cvar cl_maxpackets IN 60 125\n");
 		return;
 	}
+
 	trap_Argv(1, cvarName, sizeof(cvarName));
 	trap_Argv(2, mode, sizeof(mode));
 	trap_Argv(3, cvarValue1, sizeof(cvarValue1));
@@ -1029,6 +1031,7 @@ void CC_svcvar(void)
 	if (trap_Argc() == 5)
 	{
 		trap_Argv(4, cvarValue2, sizeof(cvarValue2));
+
 	}
 	else
 	{
@@ -1118,7 +1121,7 @@ void CC_svcvar(void)
 		level.svCvarsCount++;
 	}
 
-	G_UpdateSvCvars();
+//	G_UpdateSvCvars();   // cause of lag on map_restart...moved call after all cvars are loaded
 }
 
 /*
@@ -1215,6 +1218,11 @@ qboolean    ConsoleCommand( void ) {
 	// RTCWPro - cvar limiting
 	if (Q_stricmp(cmd, "sv_cvarempty") == 0) {
 		CC_cvarempty();
+		return qtrue;
+	}
+	if (Q_stricmp(cmd, "sv_cvarload") == 0) {
+		G_UpdateSvCvars();
+
 		return qtrue;
 	}
 	if (Q_stricmp(cmd, "sv_cvar") == 0) {
