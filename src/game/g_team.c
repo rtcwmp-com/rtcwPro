0bff97b446d6842a87c512e6cdd19c121d7181bc
krazykaze81 <mike97gt@gmail.com>
2020-12-29 11:19:42 -0500

    fixed #193 player ready status; fixed #174 weapon restrictions

--- src/game/g_team.c
+++ src/game/g_team.c
@@ -990,7 +990,7 @@ void TeamplayInfoMessage( gentity_t *ent ) {
 	stringlength = 0;
 
 	// Do each team for team information
-	for (i = 0, cnt = 0; i < level.numConnectedClients && cnt < TEAM_MAXOVERLAY; i++) {
+	for (i = 0, cnt = 0; i < level.numConnectedClients /*&& cnt < TEAM_MAXOVERLAY*/; i++) {
 
 		player = g_entities + level.sortedClients[i];
 
