c5a0ae91b7319f6d2012f868a6227f758aa5fb61
Nate 'L0 <nate@l0net.com>
2021-02-04 02:35:11 +0100

    Hooked latest libcurl version and removed old one..

---
+++ libs/libcurl/CMakeFiles/CheckIncludeFiles/HAVE_CRYPTO_H.c
@@ -0,0 +1,15 @@
+/* */
+#include <windows.h>
+#include <winsock.h>
+#include <ws2tcpip.h>
+#include <winsock2.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/utime.h>
+#include <assert.h>
+#include <crypto.h>
+
+
+int main(void){return 0;}
+
---
+++ libs/libcurl/CMakeFiles/CheckIncludeFiles/HAVE_WINSOCK2_H.c
@@ -0,0 +1,9 @@
+/* */
+#include <windows.h>
+#include <winsock.h>
+#include <ws2tcpip.h>
+#include <winsock2.h>
+
+
+int main(void){return 0;}
+
