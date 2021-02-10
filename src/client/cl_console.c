63a722ada74f4b517c137ac317b5d5b38f408cd4
Nate 'L0 <nate@l0net.com>
2021-02-10 07:59:55 +0100

    Added support for new colors in console..

--- src/client/cl_console.c
+++ src/client/cl_console.c
@@ -594,8 +594,8 @@ void Con_DrawNotify( void ) {
 			if ( ( text[x] & 0xff ) == ' ' ) {
 				continue;
 			}
-			if ( ( ( text[x] >> 8 ) & 7 ) != currentColor ) {
-				currentColor = ( text[x] >> 8 ) & 7;
+			if ( ( ( text[x] >> 8 ) & COLOR_BITS) != currentColor ) {
+				currentColor = ( text[x] >> 8 ) & COLOR_BITS;
 				re.SetColor( g_color_table[currentColor] );
 			}
 			SCR_DrawSmallChar( cl_conXOffset->integer + con.xadjust + ( x + 1 ) * SMALLCHAR_WIDTH, v, text[x] & 0xff );
@@ -748,8 +748,8 @@ void Con_DrawSolidConsole( float frac ) {
 				continue;
 			}
 
-			if ( ( ( text[x] >> 8 ) & 7 ) != currentColor ) {
-				currentColor = ( text[x] >> 8 ) & 7;
+			if ( ( ( text[x] >> 8 ) & COLOR_BITS) != currentColor ) {
+				currentColor = ( text[x] >> 8 ) & COLOR_BITS;
 				re.SetColor( g_color_table[currentColor] );
 			}
 			SCR_DrawSmallChar(  con.xadjust + ( x + 1 ) * SMALLCHAR_WIDTH, y, text[x] & 0xff );
