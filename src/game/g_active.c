70727075b5038f335816489b464a2576ac7ef4d3
wolf6542 <sivanov4824@gmail.com>
2021-08-15 19:38:17 +0300

    Revert 77b0c212a19663d3fd37558bd7a66aabc9af82f3

--- src/game/g_active.c
+++ src/game/g_active.c
@@ -444,7 +444,7 @@ void SpectatorThink( gentity_t *ent, usercmd_t *ucmd ) {
 			ucmd->upmove = 0;
 			ucmd->wbuttons = 0;
 		}
-		else if (client->sess.shoutcaster)
+		else if (client->sess.shoutcaster && client->noclip)
 		{
 			client->ps.pm_type = PM_NOCLIP;
 		}
