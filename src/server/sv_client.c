21eca2ae694d968dbaf22009ce0f77e980a9a7c3
krazykaze81 <mike97gt@gmail.com>
2021-11-10 21:22:52 -0500

    Added download message for invalid client version

--- src/server/sv_client.c
+++ src/server/sv_client.c
@@ -320,7 +320,7 @@ void SV_DirectConnect( netadr_t from ) {
 		int cl_checkversion = atoi(Info_ValueForKey(userinfo, "cl_checkversion"));
 		if (cl_checkversion != sv_checkVersion->integer)
 		{
-			NET_OutOfBandPrint(NS_SERVER, from, "print\nInvalid client version. " "Run updater as admin.\n");
+			NET_OutOfBandPrint(NS_SERVER, from, "print\nInvalid client version.\n" "Run updater as admin or download at rtcwpro.com\n");
 			return;
 		}
 	}
