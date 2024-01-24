f8aa85707f1b922bd2e7127dfc476cd408a32eb4
krazykaze81 <mike97gt@gmail.com>
2024-01-24 11:53:45 -0500

    Improve fetch-depdencies
    Stay within the rtcwpro checkout dir, build the dependencies within the deps dir, gitignore it

--- fetch-dependencies.bat
+++ fetch-dependencies.bat
@@ -96,21 +96,29 @@ rem ***************************************************************************
   )
 
 :fetchDeps
-	rem assume we are being called inside a project, don't fetch the stuff inside of the project
-	cd ..
+	rem assume we are being called inside a project, do stuff inside a directory
+	if not exist "deps" (
+		mkdir deps
+	)
+	cd deps
+
 	echo Fetching Dependencies...
 	if not exist "openssl" (
 		echo openssl...
 		call powershell "$source= ((Invoke-RestMethod -Method GET -Uri """https://api.github.com/repos/openssl/openssl/releases""")[0].assets | Where-Object name -like """openssl*.tar.gz""" ).browser_download_url;Write-Host $source;Invoke-WebRequest -Uri $source -Out $(Split-Path -Path $source -Leaf)"
 		call powershell "$file = Get-ChildItem """openssl*"""; tar -xzf $file"
 		call powershell "Get-ChildItem """openssl*""" | move-item -Destination """openssl\""
+	) else (
+		set SKIP_SSL=1
 	)
 	
 	rem OpenSSL needs perl to build, strawberry perl portable will do it, add it to PATH
 	if not exist "strawberry-perl" (
 		echo strawberry-perl...
 		call powershell "$source= ((Invoke-RestMethod -Method GET -Uri """https://api.github.com/repos/StrawberryPerl/Perl-Dist-Strawberry/releases""")[0].assets | Where-Object name -like """*portable.zip""" ).browser_download_url;Write-Host $source;Invoke-WebRequest -Uri $source -Out $(Split-Path -Path $source -Leaf)"
+		call powershell "Get-ChildItem """strawberry*portable.zip""" | move-item -Destination """strawberry-perl.zip""""
 		call powershell "Expand-Archive -Path """strawberry-perl*portable.zip""" -DestinationPath """strawberry-perl""""
+		call powershell "rm strawberry-perl.zip"
 	)
 	set PATH=%cd%\strawberry-perl\perl\bin;%PATH%
 	
@@ -121,28 +129,69 @@ rem ***************************************************************************
 		call powershell "Get-ChildItem """curl*""" | move-item -Destination """curl.zip""""
 		call powershell "Expand-Archive -Path """curl*.zip""" -DestinationPath """curl""""
 		call powershell "Get-ChildItem """curl\*\*""" | move-item -Destination """curl\""
+		call powershell "rm curl.zip"
+	) else (
+		set SKIP_CURL=1
 	)
-
+	
+	if not exist "libjpeg-turbo" (
+		echo libjpeg-turbo...
+		call powershell "$source= (Invoke-RestMethod -Method GET -Uri https://api.github.com/repos/libjpeg-turbo/libjpeg-turbo/releases)[0].zipball_url;"^
+						"Write-Host $source;"^
+						"$file=$(Split-Path -Path $source -Leaf);"^
+						"Invoke-WebRequest -Uri $source -Out $file;"^
+						"Get-ChildItem $file | move-item -Destination libjpeg-turbo.zip"
+		call powershell "Expand-Archive -Path """libjpeg-turbo*.zip""" -DestinationPath """libjpeg-turbo""""
+		call powershell "Get-ChildItem """libjpeg-turbo\*\*""" | move-item -Destination """libjpeg-turbo\""
+		call powershell "rm libjpeg-turbo.zip"
+	) else (
+		set SKIP_JPEG=1
+	)
 :buildDeps
 	call "%PF%\%VC_PATH%\VC\Auxiliary\Build\vcvars32.bat"
-	set ROOT_DEV_DIR=%cd%
-	cd curl
-	call buildconf.bat
-	cd projects
-	call generate.bat %VC_generate%
+	set ROOT_DEP_DIR=%cd%
+:buildOpenSSL
+	if defined SKIP_SSL (
+		goto buildCurl
+	)
+	cd "%ROOT_DEP_DIR%\curl\projects\"
 	call build-openssl.bat %vc_version% x86 release ..\..\openssl 
-	cd Windows\%VC_DESC%
+:buildCurl
+	if defined SKIP_CURL (
+		goto buildLibJPEG
+	)
+	
+	cd "%ROOT_DEP_DIR%\curl"
+	call buildconf.bat
+	cd "%ROOT_DEP_DIR%\curl\projects\"
+	call generate.bat %VC_generate%
+	cd "%ROOT_DEP_DIR%\curl\projects\Windows\%VC_DESC%"
 	call "%PF%\%VC_PATH%\Common7\IDE\devenv.exe" curl-all.sln /Build "DLL Release - DLL OpenSSL"
+:buildLibJPEG
+	if defined SKIP_JPEG (
+		goto harvest
+	)
+	cd "%ROOT_DEP_DIR%\libjpeg-turbo"
+	mkdir build
+	cd build
+	call cmake -G"Visual Studio 17 2022" -A Win32 -DCMAKE_BUILD_TYPE=Release ..
+	call devenv libjpeg-turbo.sln /Build Release
+	call powershell "Get-ChildItem """*.h""" | copy-item -Destination """..\""
+	
 :harvest
-	cd %ROOT_DEV_DIR%\curl
-	mkdir bin
-	call powershell "Get-ChildItem """build\Win32\%VC_DESC%\DLL Release - DLL OpenSSL\*.dll""" | copy-item -Destination """bin\""
-	call powershell "Get-ChildItem """build\Win32\%VC_DESC%\DLL Release - DLL OpenSSL\*.lib""" | copy-item -Destination """bin\""
-	call powershell "Get-ChildItem """build\Win32\%VC_DESC%\DLL Release - DLL OpenSSL\*.pdb""" | copy-item -Destination """bin\""
-	call powershell "Get-ChildItem """build\Win32\%VC_DESC%\DLL Release - DLL OpenSSL\*.exe""" | copy-item -Destination """bin\""
-	call powershell "Get-ChildItem """..\openssl\build\Win32\%VC_DESC%\DLL Release\*.dll""" | copy-item -Destination """bin\""
-	call powershell "Get-ChildItem """..\openssl\build\Win32\%VC_DESC%\DLL Release\*.lib""" | copy-item -Destination """bin\""
-	call powershell "Get-ChildItem """..\openssl\build\Win32\%VC_DESC%\DLL Release\*.pdb""" | copy-item -Destination """bin\""
-	call powershell "Get-ChildItem """..\openssl\build\Win32\%VC_DESC%\DLL Release\*.exe""" | copy-item -Destination """bin\""
-	echo Done
+	cd %ROOT_DEP_DIR%
+	if not exist "bin" (
+		mkdir bin
+	)
+	call powershell "Get-ChildItem """curl\build\Win32\%VC_DESC%\DLL Release - DLL OpenSSL\*.dll""" | copy-item -Destination """bin\""
+	call powershell "Get-ChildItem """curl\build\Win32\%VC_DESC%\DLL Release - DLL OpenSSL\*.lib""" | copy-item -Destination """bin\""
+	call powershell "Get-ChildItem """curl\build\Win32\%VC_DESC%\DLL Release - DLL OpenSSL\*.pdb""" | copy-item -Destination """bin\""
+	call powershell "Get-ChildItem """curl\build\Win32\%VC_DESC%\DLL Release - DLL OpenSSL\*.exe""" | copy-item -Destination """bin\""
+	call powershell "Get-ChildItem """openssl\build\Win32\%VC_DESC%\DLL Release\*.dll""" | copy-item -Destination """bin\""
+	call powershell "Get-ChildItem """openssl\build\Win32\%VC_DESC%\DLL Release\*.lib""" | copy-item -Destination """bin\""
+	call powershell "Get-ChildItem """openssl\build\Win32\%VC_DESC%\DLL Release\*.pdb""" | copy-item -Destination """bin\""
+	call powershell "Get-ChildItem """openssl\build\Win32\%VC_DESC%\DLL Release\*.exe""" | copy-item -Destination """bin\""
+	call powershell "Get-ChildItem """libjpeg-turbo\build\Release\*.dll""" | copy-item -Destination """bin\""
+	call powershell "Get-ChildItem """libjpeg-turbo\build\Release\*.lib""" | copy-item -Destination """bin\""
+	echo Copy the DLL files from deps/bin to your RtcwPro install location where wolfMP.exe is
 	pause
