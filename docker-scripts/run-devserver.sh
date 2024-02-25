7830a9e72c868d0bc1ce6b4604195e8b968b3a3d
snappas <snappas@gmail.com>
2024-02-25 12:28:01 -0600

    Cross compile fixes

--- docker-scripts/run-devserver.sh
+++ docker-scripts/run-devserver.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 addrs=(`hostname -I`)
-docker run --mount type=bind,src=`pwd`../build,dst=/home/game/dev   -p "${addrs[0]}:27960:27960/udp"   -e "MAPS=adlernest_b3:te_escape2:te_frostbite"   -e "PASSWORD=war"   -e "REFEREEPASSWORD=pass123" -e "SERVERCONF=comp" rtcwpro/server-dev:1.0
+docker run --mount type=bind,src=`pwd`/../build,dst=/home/game/dev   -p "${addrs[0]}:27960:27960/udp"   -e "MAPS=adlernest_b3:te_escape2:te_frostbite"   -e "PASSWORD=war"   -e "REFEREEPASSWORD=pass123" -e "SERVERCONF=comp" rtcwpro/server-dev:1.0
