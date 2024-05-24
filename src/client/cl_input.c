9be244837b7b34a0307dbe5637d3c36a60fb23df
snappas <snappas@gmail.com>
2024-05-24 12:04:33 -0500

    Squashed commit of the following:
    commit ffa7e21ca60afcc050afd96f6f25bcb151cd6abf
    Author: snappas <snappas@gmail.com>
    Date:   Wed May 22 19:36:03 2024 -0500
    
        Allow cvar restriction for old client's com_maxfps
    
    commit 15bb418e6d43d65a85b72146718f36b4396c79ee
    Author: snappas <snappas@gmail.com>
    Date:   Sun May 19 17:19:42 2024 -0500
    
        Fix overwriting the cmd's viewangles
    
    commit 0308ad761c40a529ef11e46f81b357cea07dcbd0
    Author: snappas <snappas@gmail.com>
    Date:   Sun May 19 12:32:19 2024 -0500
    
        Fix weapon switching overwriting the cmd's weapon
    
    commit 62a527dacffbe98e6a358bdaf5a5ff9339df1115
    Author: snappas <snappas@gmail.com>
    Date:   Sat May 18 23:21:56 2024 -0500
    
        Re-add memory leak fix removed by merge conflict
    
    commit 6f9ee6ada9dadd15b6fa4a303bb2836337ace024
    Author: snappas <snappas@gmail.com>
    Date:   Sat May 18 22:57:07 2024 -0500
    
        Build user commands once per fixed timestep
    
    commit 2bad92342558032c680c5cfe849687a79c68d356
    Merge: 32252874 bd2a5036
    Author: snappas <snappas@gmail.com>
    Date:   Thu May 16 18:47:49 2024 -0500
    
        Merge branch 'develop' into feature/500fps
    
    commit 32252874df2fafdf65a187ddd2774f3ce853b126
    Author: snappas <snappas@gmail.com>
    Date:   Sat Mar 23 23:48:15 2024 -0500
    
        Fix valgrind errors
    
    commit 21bfb6e34804e20be7316aeac4ad1384de8fcd7a
    Author: snappas <snappas@gmail.com>
    Date:   Sat Mar 23 23:40:01 2024 -0500
    
        Replace x86 assembly in several functions
    
    commit b1e48b5fe8c349d2cbb6d578e4cb368cac8fe3ee
    Author: snappas <snappas@gmail.com>
    Date:   Sat Mar 23 00:33:52 2024 -0500
    
        Fix issue with choppy jump deceleration
    
    commit f428d442e9a38658bf3acc5f1f18c1a78a2f7060
    Merge: 2cc2aa87 cec136ec
    Author: snappas <snappas@users.noreply.github.com>
    Date:   Fri Mar 22 17:11:45 2024 -0500
    
        Merge pull request #438 from rtcwmp-com/develop
    
        Add build image for Ubuntu 18
    
    commit 2cc2aa8746e26d37d9215fc1850b2a1b44b8947f
    Author: snappas <snappas@gmail.com>
    Date:   Thu Mar 21 20:54:36 2024 -0500
    
        Send packets at fixed interval
    
    commit 9d2f6f703ded3a04a034e5fc12d69de60592672e
    Author: snappas <snappas@gmail.com>
    Date:   Mon Mar 18 19:41:13 2024 -0500
    
        Render separately from physics timestep

--- src/client/cl_input.c
+++ src/client/cl_input.c
@@ -615,7 +615,9 @@ void CL_FinishMove( usercmd_t *cmd ) {
 	int i;
 
 	// copy the state that the cgame is currently sending
-	cmd->weapon = cl.cgameUserCmdValue;
+	if (!(cmd->weapon)) { // dont overwrite this every rendered frame 
+		cmd->weapon = cl.cgameUserCmdValue;
+	}
 
 	cmd->holdable = cl.cgameUserHoldableValue;  //----(SA)	modified
 
@@ -626,8 +628,10 @@ void CL_FinishMove( usercmd_t *cmd ) {
 	// can be determined without allowing cheating
 	cmd->serverTime = cl.serverTime;
 
-	for ( i = 0 ; i < 3 ; i++ ) {
-		cmd->angles[i] = ANGLE2SHORT( cl.viewangles[i] );
+	if (!(cmd->angles[0]) && !(cmd->angles[1]) && !(cmd->angles[2])) {
+		for (i = 0; i < 3; i++) {
+			cmd->angles[i] = ANGLE2SHORT(cl.viewangles[i]);
+		}
 	}
 }
 
@@ -647,18 +651,18 @@ usercmd_t CL_CreateCmd( void ) {
 	// keyboard angle adjustment
 	CL_AdjustAngles();
 
-	memset( &cmd, 0, sizeof( cmd ) );
+	
 
-	CL_CmdButtons( &cmd );
+	CL_CmdButtons( &cl.currentCmd );
 
 	// get basic movement from keyboard
-	CL_KeyMove( &cmd );
+	CL_KeyMove( &cl.currentCmd);
 
 	// get basic movement from mouse
-	CL_MouseMove( &cmd );
+	CL_MouseMove( &cl.currentCmd);
 
 	// get basic movement from joystick
-	CL_JoystickMove( &cmd );
+	CL_JoystickMove( &cl.currentCmd);
 
 	// check to make sure the angles haven't wrapped
 	if ( cl.viewangles[PITCH] - oldAngles[PITCH] > 90 ) {
@@ -676,7 +680,7 @@ usercmd_t CL_CreateCmd( void ) {
 	cl_recoilPitch->value = 0;
 
 	// store out the final values
-	CL_FinishMove( &cmd );
+	CL_FinishMove( &cl.currentCmd);
 
 	// draw debug graphs of turning for mouse testing
 	if ( cl_debugMove->integer ) {
@@ -688,7 +692,7 @@ usercmd_t CL_CreateCmd( void ) {
 		}
 	}
 
-	return cmd;
+	return cl.currentCmd;
 }
 
 
@@ -719,10 +723,10 @@ void CL_CreateNewCommands( void ) {
 
 
 	// generate a command for this frame
-	cl.cmdNumber++;
+	
 	cmdNum = cl.cmdNumber & CMD_MASK;
 	cl.cmds[cmdNum] = CL_CreateCmd();
-	cmd = &cl.cmds[cmdNum];
+
 }
 
 /*
@@ -939,9 +943,6 @@ void CL_SendCmd( void ) {
 	if ( com_sv_running->integer && sv_paused->integer && cl_paused->integer ) {
 		return;
 	}
-
-	// we create commands even if a demo is playing,
-	CL_CreateNewCommands();
 
 	// don't send a packet if the last packet was sent too recently
 	if ( !CL_ReadyToSendPacket() ) {
