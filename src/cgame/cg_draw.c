rtcwmp-com/rtcwPro

commit f6d06f0389bc9de53ea23eec08511983a6fcc7ef

show SMG ammo on crosshair info

src/game/g_client.c
@@ -1056,9 +1056,11 @@ void SetWolfSpawnWeapons( gentity_t *ent ) {
 			// JPW NERVE -- if LT is selected but illegal weapon, set to team-specific SMG
 			if ( ( pc == PC_LT ) && ( client->sess.playerWeapon > 5 ) ) {
 				if ( client->sess.sessionTeam == TEAM_RED ) {
-					client->sess.playerWeapon = 3;
+					client->sess.playerWeapon = WP_MP40;
+					client->pers.mainWeapon = WP_MP40;
 				} else {
-					client->sess.playerWeapon = 4;
+					client->sess.playerWeapon = WP_THOMPSON;
+					client->pers.mainWeapon = WP_THOMPSON;
 				}
 			}
 			// jpw
@@ -1073,6 +1075,7 @@ void SetWolfSpawnWeapons( gentity_t *ent ) {
 						client->ps.ammo[BG_FindAmmoForWeapon( WP_MP40 )] += (32 * ltClips);
 					}
 					client->ps.weapon = WP_MP40;
+					client->pers.mainWeapon = WP_MP40;
 					break;
 
 				case 4:     // WP_THOMPSON
@@ -1084,6 +1087,7 @@ void SetWolfSpawnWeapons( gentity_t *ent ) {
 						client->ps.ammo[BG_FindAmmoForWeapon( WP_THOMPSON )] += (30 * ltClips);
 					}
 					client->ps.weapon = WP_THOMPSON;
+					client->pers.mainWeapon = WP_THOMPSON;
 					break;
 
 				case 5:     // WP_STEN
@@ -1095,6 +1099,7 @@ void SetWolfSpawnWeapons( gentity_t *ent ) {
 						client->ps.ammo[BG_FindAmmoForWeapon( WP_STEN )] += (32 * ltClips);
 					}
 					client->ps.weapon = WP_STEN;
+					client->pers.mainWeapon = WP_STEN;
 					break;
 
 
@@ -1124,6 +1129,7 @@ void SetWolfSpawnWeapons( gentity_t *ent ) {
 					client->ps.ammoclip[BG_FindClipForWeapon( WP_MAUSER )] = 10;
 					client->ps.ammo[BG_FindAmmoForWeapon( WP_MAUSER )] = 10;
 					client->ps.weapon = WP_MAUSER;
+					client->pers.mainWeapon = WP_MAUSER;
 					break;
 
 				case 8:     // WP_PANZERFAUST
@@ -1142,6 +1148,7 @@ void SetWolfSpawnWeapons( gentity_t *ent ) {
 					COM_BitSet( client->ps.weapons, WP_PANZERFAUST );
 					client->ps.ammo[BG_FindAmmoForWeapon( WP_PANZERFAUST )] = 4;
 					client->ps.weapon = WP_PANZERFAUST;
+					client->pers.mainWeapon = WP_PANZERFAUST;
 					break;
 
 				case 9:     // WP_VENOM
@@ -1160,6 +1167,7 @@ void SetWolfSpawnWeapons( gentity_t *ent ) {
 					COM_BitSet( client->ps.weapons, WP_VENOM );
 					client->ps.ammoclip[BG_FindAmmoForWeapon( WP_VENOM )] = 500;
 					client->ps.weapon = WP_VENOM;
+					client->pers.mainWeapon = WP_VENOM;
 					break;
 
 				case 10:    // WP_FLAMETHROWER
@@ -1173,16 +1181,12 @@ void SetWolfSpawnWeapons( gentity_t *ent ) {
 							SetDefaultWeapon(client, qtrue);
 							break;
 						}
-
-						if (client->pers.restrictedWeapon != WP_FLAMETHROWER) {
-							(client->sess.sessionTeam == TEAM_RED) ? level.axisFlamer++ : level.alliedFlamer++;
-							client->pers.restrictedWeapon = WP_FLAMETHROWER;
-						}
 					}
 
 					COM_BitSet( client->ps.weapons, WP_FLAMETHROWER );
 					client->ps.ammoclip[BG_FindAmmoForWeapon( WP_FLAMETHROWER )] = 200;
 					client->ps.weapon = WP_FLAMETHROWER;
+					client->pers.mainWeapon = WP_FLAMETHROWER;
 					break;
 
 				default:    // give MP40 if given invalid weapon number
@@ -1195,6 +1199,7 @@ void SetWolfSpawnWeapons( gentity_t *ent ) {
 							client->ps.ammo[BG_FindAmmoForWeapon( WP_MP40 )] += (32 * ltClips);
 						}
 						client->ps.weapon = WP_MP40;
+						client->pers.mainWeapon = WP_MP40;
 					} else { // TEAM_BLUE
 						COM_BitSet( client->ps.weapons, WP_THOMPSON );
 						client->ps.ammoclip[BG_FindClipForWeapon( WP_THOMPSON )] += 30;
@@ -1204,6 +1209,7 @@ void SetWolfSpawnWeapons( gentity_t *ent ) {
 							client->ps.ammo[BG_FindAmmoForWeapon( WP_THOMPSON )] += (30 * ltClips);
 						}
 						client->ps.weapon = WP_THOMPSON;
+						client->pers.mainWeapon = WP_THOMPSON;
 					}
 					break;
 			}

src/game/g_local.h
@@ -659,6 +659,7 @@ typedef struct {
 	int	samplehead;
 	unsigned int pingsample_counter;
 	int deathYaw;
+	int mainWeapon;
 } clientPersistant_t;
 
 // L0 - antilag port

src/game/g_team.c
@@ -1052,8 +1052,8 @@ void TeamplayInfoMessage(team_t team)
 			{
 				displayHealth = 0;
 			}
-
-			playerWeapon = player->client->ps.weapon;
+			
+			playerWeapon = player->client->pers.mainWeapon; // use new pers var becasue player->client->ps.weapon is current weapon being used
 			playerAmmoClip = player->client->ps.ammoclip[BG_FindAmmoForWeapon(playerWeapon)];
 			playerAmmo = player->client->ps.ammo[BG_FindAmmoForWeapon(playerWeapon)];
 			playerNades += player->client->ps.ammoclip[BG_FindClipForWeapon(WP_GRENADE_LAUNCHER)];

commit 962e8e28207cef4d7e73698e705c16ff5e2b4759

Squashed commit of the following:
commit 874ec2b18188f42fc51d143b93e3f94c382a902f
Author: krazykaze81 <mike97gt@gmail.com>
Date:   Thu Apr 10 14:53:24 2025 -0400

    fix kill feed issue with team obituary colors

commit 4e31d725346616c021c88482a4497c10a4f508d8
Author: krazykaze81 <mike97gt@gmail.com>
Date:   Wed Mar 26 13:01:32 2025 -0400

    fixed weapon restrictions broke by f6d06f0

commit d36e8fbfeb461c72b27c24c0a7dbd065a1bfcaea
Author: krazykaze81 <mike97gt@gmail.com>
Date:   Tue Mar 4 15:22:08 2025 -0500

    added g_allowReadyTeam to control readyteam command

commit 5b37526c54f918f0779fb61a96dd8aa736817bc8
Author: krazykaze81 <mike97gt@gmail.com>
Date:   Fri Feb 21 12:10:57 2025 -0500

    fixed team obituary colors

commit 2011822b875f16eeb3deb2a2059e73410b08468d
Author: krazykaze81 <mike97gt@gmail.com>
Date:   Wed Jan 8 10:45:58 2025 -0500

    code cleanup for old antilag

commit 86a1a7f58ab1eb5a3e2b7c49a318786d82e39e0b
Author: krazykaze81 <mike97gt@gmail.com>
Date:   Wed Jan 8 10:42:59 2025 -0500

    added cg_drawCI cvar for high fps/ping players to toggle phonejack icon

commit 448c547969d92b347b809ba56af921532a8127cd
Author: krazykaze81 <mike97gt@gmail.com>
Date:   Tue Jan 7 09:18:31 2025 -0500

    updated cvarlist

commit 294b96a321d4a528194b248786e4faf422380d93
Merge: 6696f48d 006499e8
Author: krazykaze81 <mike97gt@gmail.com>
Date:   Mon Dec 16 14:37:02 2024 -0500

    Merge branch 'develop' of https://github.com/rtcw-nihi/ospx into develop

commit 6696f48d97816581e760df87748466f3104dec84
Author: krazykaze81 <mike97gt@gmail.com>
Date:   Mon Dec 16 14:36:51 2024 -0500

    draw RT during warmup

src/cgame/cg_draw.c
@@ -1078,9 +1078,7 @@ static float CG_DrawRespawnTimer(float y) {
 		return y;
 	}
 
-	if (cgs.gamestate != GS_PLAYING)
-		str = "";
-	else if (cgs.clientinfo[cg.snap->ps.clientNum].team == TEAM_SPECTATOR)
+	if (cgs.clientinfo[cg.snap->ps.clientNum].team == TEAM_SPECTATOR)
 		str = "";
 	else if (cgs.clientinfo[cg.snap->ps.clientNum].team == TEAM_RED)
 		str = va("RT: %-2d", CG_CalculateReinfTime(qfalse));
@@ -1319,11 +1317,7 @@ static float CG_DrawProRespawnTimer(float y) {
 		return y;
 	}
 
-	if (cgs.gamestate != GS_PLAYING) {
-		return y;
-	}
-
-	if (cgs.clientinfo[cg.clientNum].shoutStatus) {
+	if (cgs.clientinfo[cg.clientNum].shoutStatus || cgs.clientinfo[cg.snap->ps.clientNum].team == TEAM_SPECTATOR) {
 		return y;
 	}
 
@@ -1866,7 +1860,8 @@ static void CG_DrawDisconnect( void ) {
 	x = 640 - 72;
 	y = 480 - 52;
 
-	CG_DrawPic( x, y, 48, 48, trap_R_RegisterShader( "gfx/2d/net.tga" ) );
+	if (cg_drawCI.integer)
+		CG_DrawPic( x, y, 48, 48, trap_R_RegisterShader( "gfx/2d/net.tga" ) );
 }
 
 

src/cgame/cg_event.c
@@ -82,10 +82,14 @@ const char  *CG_PlaceString( int rank ) {
 	return str;
 }
 
-ID_INLINE void CG_ColorObituaryEntName(clientInfo_t* ci, char* name, qboolean teamKill)
+ID_INLINE void CG_ColorObituaryEntName(clientInfo_t *ci, char *name, qboolean teamKill)
 {
 	clientInfo_t* self = &cgs.clientinfo[cg.clientNum];
-	Q_CleanStr(name);
+
+	char cleanName[MAX_NETNAME];
+	BG_cleanName(name, cleanName, MAX_NETNAME, qfalse);
+	Q_strncpyz(name, cleanName, sizeof(cleanName));
+
 	memmove(name + 2, name, strlen(name) + 1);
 	name[0] = '^';
 
@@ -189,12 +193,15 @@ static void CG_Obituary( entityState_t *ent ) {
 	}
 	Q_strncpyz( targetName, Info_ValueForKey( targetInfo, "n" ), sizeof( targetName ) - 2 );
 
+	char targetNameCopy[MAX_NETNAME];
+	Q_strncpyz(targetNameCopy, targetName, sizeof(targetName));
+
 	if (cg_teamObituaryColors.integer)
 	{
-		CG_ColorObituaryEntName(ci, targetName, ci->team == ca->team);
+		CG_ColorObituaryEntName(ci, targetNameCopy, ci->team == ca->team);
 	}
 
-	strcat( targetName, S_COLOR_WHITE );
+	strcat(targetNameCopy, S_COLOR_WHITE );
 
 	message2 = "";
 
@@ -282,7 +289,7 @@ static void CG_Obituary( entityState_t *ent ) {
 	}
 	if ( message ) {
 		message = CG_TranslateString( message );
-		CG_Printf( "[cgnotify]%s %s.\n", targetName, message );
+		CG_Printf( "[cgnotify]%s %s.\n", targetNameCopy, message );
 		return;
 	}
 
@@ -291,14 +298,14 @@ static void CG_Obituary( entityState_t *ent ) {
 		char    *s;
 
 		if ( cgs.gametype < GT_TEAM ) {
-			s = va( "You killed %s\n%s place with %i", targetName,
+			s = va( "You killed %s\n%s place with %i", targetNameCopy,
 					CG_PlaceString( cg.snap->ps.persistant[PERS_RANK] + 1 ),
 					cg.snap->ps.persistant[PERS_SCORE] );
 		} else {
 			if ( ci->team == ca->team ) {
-				s = va( "%s %s", CG_TranslateString( "You killed ^1TEAMMATE^7" ), targetName );
+				s = va( "%s %s", CG_TranslateString( "You killed ^1TEAMMATE^7" ), targetNameCopy);
 			} else {
-				s = va( "%s %s", CG_TranslateString( "You killed" ), targetName );
+				s = va( "%s %s", CG_TranslateString( "You killed" ), targetNameCopy);
 			}
 		}
 
@@ -489,7 +496,7 @@ static void CG_Obituary( entityState_t *ent ) {
 			if ( message2 ) {
 				message2 = CG_TranslateString( message2 );
 			}
-			CG_Printf( "[cgnotify]%s %s %s%s\n", targetName, message, attackerName, message2 );
+			CG_Printf( "[cgnotify]%s %s %s%s\n", targetNameCopy, message, attackerName, message2 );
 			return;
 		}
 	}
@@ -498,10 +505,10 @@ static void CG_Obituary( entityState_t *ent ) {
 // JPW NERVE added mod check for machinegun (prolly mortar here too)
 	switch ( mod ) {
 	case MOD_MACHINEGUN:
-		CG_Printf( "[cgnotify]%s was riddled by machinegun fire\n",targetName );
+		CG_Printf( "[cgnotify]%s was riddled by machinegun fire\n", targetNameCopy);
 		break;
 	default:
-		CG_Printf( "[cgnotify]%s died.\n", targetName );
+		CG_Printf( "[cgnotify]%s died.\n", targetNameCopy);
 		break;
 	}
 // jpw

src/cgame/cg_local.h
@@ -2276,6 +2276,8 @@ extern vmCvar_t cg_teamObituaryColorSameTK;		// same team TK color
 extern vmCvar_t cg_teamObituaryColorEnemy;		// enemy team color
 extern vmCvar_t cg_teamObituaryColorEnemyTK;	// enemy team TK color
 
+extern vmCvar_t cg_drawCI;
+
 //
 // cg_main.c
 //

src/cgame/cg_main.c
@@ -468,6 +468,8 @@ vmCvar_t cg_customCrosshairVMirror;
 vmCvar_t cg_customCrosshairXGap;
 vmCvar_t cg_customCrosshairYGap;
 
+vmCvar_t cg_drawCI;
+
 typedef struct {
 	vmCvar_t    *vmCvar;
 	char        *cvarName;
@@ -833,7 +835,9 @@ cvarTable_t cvarTable[] = {
 	{ &cg_customCrosshairYOffset, "cg_customCrosshairYOffset", "0", CVAR_ARCHIVE },
 	{ &cg_customCrosshairVMirror, "cg_customCrosshairVMirror", "1", CVAR_ARCHIVE },
 	{ &cg_customCrosshairXGap, "cg_customCrosshairXGap", "0", CVAR_ARCHIVE },
-	{ &cg_customCrosshairYGap, "cg_customCrosshairYGap", "0", CVAR_ARCHIVE }
+	{ &cg_customCrosshairYGap, "cg_customCrosshairYGap", "0", CVAR_ARCHIVE },
+
+	{ &cg_drawCI, "cg_drawCI", "1", CVAR_ARCHIVE }
 	
 };
 int cvarTableSize = sizeof( cvarTable ) / sizeof( cvarTable[0] );

src/game/g_active.c
@@ -2399,16 +2399,6 @@ void ClientEndFrame( gentity_t *ent ) {
 	}
 	// End ETL port
 
-	/*
-	// add the EF_CONNECTION flag if we haven't gotten commands recently
-	if (level.time - ent->client->lastCmdTime > 1000) {
-		ent->s.eFlags |= EF_CONNECTION;
-	}
-	else {
-		ent->s.eFlags &= ~EF_CONNECTION;
-	}
-	*/
-
 	ent->client->ps.stats[STAT_HEALTH] = ent->health;   // FIXME: get rid of ent->health...
 
 	G_SetClientSound( ent );
@@ -2417,6 +2407,14 @@ void ClientEndFrame( gentity_t *ent ) {
 
 	if (g_antilag.integer < 2) // Nobo antilag or off
 	{
+		// add the EF_CONNECTION flag if we haven't gotten commands recently
+		if (level.time - ent->client->lastCmdTime > 1000) {
+			ent->s.eFlags |= EF_CONNECTION;
+		}
+		else {
+			ent->s.eFlags &= ~EF_CONNECTION;
+		}
+
 		// RTCWPro
 		// Ridah, fixes jittery zombie movement
 		if (g_smoothClients.integer) {

src/game/g_client.c
@@ -1056,9 +1056,9 @@ void SetWolfSpawnWeapons( gentity_t *ent ) {
 			// JPW NERVE -- if LT is selected but illegal weapon, set to team-specific SMG
 			if ( ( pc == PC_LT ) && ( client->sess.playerWeapon > 5 ) ) {
 				if ( client->sess.sessionTeam == TEAM_RED ) {
-					client->sess.playerWeapon = WP_MP40;
+					client->sess.playerWeapon = WM_WEAPON_MP40; // 3
 				} else {
-					client->sess.playerWeapon = WP_THOMPSON;
+					client->sess.playerWeapon = WM_WEAPON_THOMPSON; // 4
 				}
 			}
 			// jpw
@@ -1073,7 +1073,7 @@ void SetWolfSpawnWeapons( gentity_t *ent ) {
 						client->ps.ammo[BG_FindAmmoForWeapon( WP_MP40 )] += (32 * ltClips);
 					}
 					client->ps.weapon = WP_MP40;
-					client->sess.playerWeapon = WP_MP40;
+					client->sess.mainWeapon = WP_MP40;
 					break;
 
 				case 4:     // WP_THOMPSON
@@ -1085,7 +1085,7 @@ void SetWolfSpawnWeapons( gentity_t *ent ) {
 						client->ps.ammo[BG_FindAmmoForWeapon( WP_THOMPSON )] += (30 * ltClips);
 					}
 					client->ps.weapon = WP_THOMPSON;
-					client->sess.playerWeapon = WP_THOMPSON;
+					client->sess.mainWeapon = WP_THOMPSON;
 					break;
 
 				case 5:     // WP_STEN
@@ -1097,7 +1097,7 @@ void SetWolfSpawnWeapons( gentity_t *ent ) {
 						client->ps.ammo[BG_FindAmmoForWeapon( WP_STEN )] += (32 * ltClips);
 					}
 					client->ps.weapon = WP_STEN;
-					client->sess.playerWeapon = WP_STEN;
+					client->sess.mainWeapon = WP_STEN;
 					break;
 
 
@@ -1127,7 +1127,7 @@ void SetWolfSpawnWeapons( gentity_t *ent ) {
 					client->ps.ammoclip[BG_FindClipForWeapon( WP_MAUSER )] = 10;
 					client->ps.ammo[BG_FindAmmoForWeapon( WP_MAUSER )] = 10;
 					client->ps.weapon = WP_MAUSER;
-					client->sess.playerWeapon = WP_MAUSER;
+					client->sess.mainWeapon = WP_MAUSER;
 					break;
 
 				case 8:     // WP_PANZERFAUST
@@ -1146,7 +1146,7 @@ void SetWolfSpawnWeapons( gentity_t *ent ) {
 					COM_BitSet( client->ps.weapons, WP_PANZERFAUST );
 					client->ps.ammo[BG_FindAmmoForWeapon( WP_PANZERFAUST )] = 4;
 					client->ps.weapon = WP_PANZERFAUST;
-					client->sess.playerWeapon = WP_PANZERFAUST;
+					client->sess.mainWeapon = WP_PANZERFAUST;
 					break;
 
 				case 9:     // WP_VENOM
@@ -1165,7 +1165,7 @@ void SetWolfSpawnWeapons( gentity_t *ent ) {
 					COM_BitSet( client->ps.weapons, WP_VENOM );
 					client->ps.ammoclip[BG_FindAmmoForWeapon( WP_VENOM )] = 500;
 					client->ps.weapon = WP_VENOM;
-					client->sess.playerWeapon = WP_VENOM;
+					client->sess.mainWeapon = WP_VENOM;
 					break;
 
 				case 10:    // WP_FLAMETHROWER
@@ -1184,7 +1184,7 @@ void SetWolfSpawnWeapons( gentity_t *ent ) {
 					COM_BitSet( client->ps.weapons, WP_FLAMETHROWER );
 					client->ps.ammoclip[BG_FindAmmoForWeapon( WP_FLAMETHROWER )] = 200;
 					client->ps.weapon = WP_FLAMETHROWER;
-					client->sess.playerWeapon = WP_FLAMETHROWER;
+					client->sess.mainWeapon = WP_FLAMETHROWER;
 					break;
 
 				default:    // give MP40 if given invalid weapon number
@@ -1197,7 +1197,7 @@ void SetWolfSpawnWeapons( gentity_t *ent ) {
 							client->ps.ammo[BG_FindAmmoForWeapon( WP_MP40 )] += (32 * ltClips);
 						}
 						client->ps.weapon = WP_MP40;
-						client->sess.playerWeapon = WP_MP40;
+						client->sess.mainWeapon = WP_MP40;
 					} else { // TEAM_BLUE
 						COM_BitSet( client->ps.weapons, WP_THOMPSON );
 						client->ps.ammoclip[BG_FindClipForWeapon( WP_THOMPSON )] += 30;
@@ -1207,7 +1207,7 @@ void SetWolfSpawnWeapons( gentity_t *ent ) {
 							client->ps.ammo[BG_FindAmmoForWeapon( WP_THOMPSON )] += (30 * ltClips);
 						}
 						client->ps.weapon = WP_THOMPSON;
-						client->sess.playerWeapon = WP_THOMPSON;
+						client->sess.mainWeapon = WP_THOMPSON;
 					}
 					break;
 			}

src/game/g_local.h
@@ -577,6 +577,8 @@ typedef struct {
 	int clientFlags;		// Sort some stuff based upon user settings
 	int specSpeed;
 	char* lastChatText;		// prevent spam chat text being saved to stats gamelog
+
+	int mainWeapon;	// keep track of players main WP_WEAPON type for player ammo counts // sess.playerWeapon is storing mp_weapon values
 } clientSession_t;
 
 //
@@ -1773,6 +1775,7 @@ extern vmCvar_t	g_ssWaitTime;
 extern vmCvar_t	g_broadcastClients;
 extern vmCvar_t g_logConfigStringChanges;
 extern vmCvar_t g_playPauseMusic;
+extern vmCvar_t g_allowReadyTeam;
 
 // unlagged
 extern vmCvar_t	g_delagHitscan;

src/game/g_main.c
@@ -268,7 +268,8 @@ vmCvar_t g_pistolClips;		// How many clips pistol gets
 vmCvar_t g_maxTeamPF;		// Max Pf's per team
 vmCvar_t g_maxTeamSniper;	// Max snipers per team
 vmCvar_t g_maxTeamVenom;	// Max venoms per team
-vmCvar_t g_maxTeamFlamer;	// Max flamers per team
+vmCvar_t g_maxTeamFlamer;	// Max flamers per 
+vmCvar_t g_allowReadyTeam;  // Allow readyteam command
 
 vmCvar_t g_antiWarp;
 vmCvar_t g_dropWeapons;			// allow drop weapon for each class, bitflag value: 1 - soldier, 2 - eng, 4 - medic, 8 - lt, default 9
@@ -556,6 +557,8 @@ cvarTable_t gameCvarTable[] = {
 	{ &g_logConfigStringChanges, "g_logConfigStringChanges", "0", CVAR_ARCHIVE, 0, qfalse },
 	{ &g_playPauseMusic, "g_playPauseMusic", "1", CVAR_ARCHIVE, 0, qfalse },
 	{ &P, "P", "", CVAR_SERVERINFO | CVAR_ARCHIVE, 0, qfalse }, // ET Port Players server info
+	{ &g_allowReadyTeam, "g_allowReadyTeam", "0", CVAR_ARCHIVE, 0, qfalse },// Allow readyteam command
+
 
 	// unlagged
 	{ &g_delagHitscan, "g_delagHitscan", "1", CVAR_ARCHIVE | CVAR_SERVERINFO, 0, qtrue },

src/game/g_players.c
@@ -574,6 +574,10 @@ void pCmd_teamReady(gentity_t *ent, qboolean ready) {
 	if (!g_tournament.integer) {
 		return;
 	}
+	if (!g_allowReadyTeam.integer) {
+		CP("print \"ReadyTeam command is not enabled on this server.\n\"");
+		return;
+	}
 	if (team_nocontrols.integer) {
 		CP("print \"Team commands are not enabled on this server.\n\"");
 		return;

src/game/g_team.c
@@ -1053,7 +1053,7 @@ void TeamplayInfoMessage(team_t team)
 				displayHealth = 0;
 			}
 			
-			playerWeapon = player->client->sess.playerWeapon; // use new pers var becasue player->client->ps.weapon is current weapon being used
+			playerWeapon = player->client->sess.mainWeapon; // use new pers var becasue player->client->ps.weapon is current weapon being used
 			playerAmmoClip = player->client->ps.ammoclip[BG_FindAmmoForWeapon(playerWeapon)];
 			playerAmmo = player->client->ps.ammo[BG_FindAmmoForWeapon(playerWeapon)];
 			playerNades += player->client->ps.ammoclip[BG_FindClipForWeapon(WP_GRENADE_LAUNCHER)];